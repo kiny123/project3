a9102775159131c2ec93a27282122c26f3cd151a
Kiny <89192734912nik@gmail.com>
2022-12-14 00:26:49 +0300

    feature-question-1

---
+++ project1/project1/DetailViewController.swift
@@ -0,0 +1,61 @@
+//
+//  DetailViewController.swift
+//  project1
+//
+//  Created by nikita on 07.12.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var ImageView: UIImageView!
+    var selectedImage: String?
+    var selectedPictureNumber = 0
+    var totalPictures = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "\(selectedPictureNumber)/\(totalPictures)"
+        navigationItem.largeTitleDisplayMode = .never
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
+
+        if let imageToLoad = selectedImage {
+            ImageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false 
+    }
+    @objc func shareTapped() {
+        guard let image = ImageView.image?.jpegData(compressionQuality: 0.8)
+        else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: ["\(selectedImage!)"], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
